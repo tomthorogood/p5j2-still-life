59e82b501be49f3a2593b280dc8d9868793e49e6
Tom Thorogood <tom@tomthorogood.com>
2022-03-20 09:23:10 -0700

    Finish 'Subject' refactor.

--- sketch.js
+++ sketch.js
@@ -3,6 +3,58 @@
 let config = null;
 let generating = false;
 
+class Layer {
+    constructor(name, width = 0, height = 0, paintFn)  {
+        this.width = width;
+        this.height = height;
+        this.name = name;
+        this.paintFn = paintFn;
+        this.pg = createGraphics(width, height);
+    }
+
+    draw() {
+        // Draws this layer onto the provided layer
+        if (this.paintFn) {
+            this.paintFn(this.pg);
+        }
+        return this.pg;
+    }
+
+    createGraphics() {
+        return createGraphics(this.width, this.height);
+    }
+
+    reset() {
+        this.clear();
+        this.pg = createGraphics(this.width, this.height);
+    }
+
+    getMaskedImage(target) {
+        let pImg;
+        ( pImg = this.pg.get() ).mask( target.get() );
+        return pImg;
+    }
+
+    clear() {
+        this.pg.remove();
+    }
+}
+
+class OptionsTemplate {
+    constructor(options) {
+        this.options = options;
+    }
+
+    getOptions() {
+        let result = {}
+        Object.keys(this.options).forEach(key => {
+            result[key] = this.options[key]();
+        });
+        return result;
+    }
+}
+
+
 class GeneratedValue {
     constructor(generator_) {
         this.generator = generator_;
@@ -18,40 +70,54 @@ class GeneratedValue {
 }
 
 
-class PatternConfig {
-    constructor() {
-        let self = this;
-        this.patterns = [
-            {
-                name: 'polkadots',
-                defaultWidth: 50,
-                defaultHeight: 50,
-                defaultShade: new GeneratedValue(getRandomColor),
-                generator: function(image, startX = 0, startY = 0, options = null) {
-                    let width = this.defaultWidth;
-                    let height = this.defaultHeight;
-                    let posX = startX;
-                    let posY = startY;
-                    while (posY < image.height) {
-                        while (posX < image.width) {
-                            let dotColor = getRandomPaletteColor(this.defaultShade.getValue());
-                            dotColor.setAlpha(185);
-                            posX += width;
-                            image.push();
-                            image.fill(dotColor);
-                            image.noStroke();
-                            image.ellipse(posX, posY, width, height);
-                            image.pop();
-                        }
-                        posX = 0;
-                        posY += height;
-                    }
-                }
+class Pattern {
+    constructor(name, optionsTemplate) {
+        this.optionsTemplate = optionsTemplate;
+        this.name = name
+    }
+
+    draw(pg, options) { throw Error('Not implemented for pattern ' + this.name) }
+
+    tile(pg, options) {
+        if (options === undefined || options === null) {
+            console.log("No options passed for pattern " + this.name);
+            options = this.optionsTemplate.getOptions();
+        }
+        while (options.posY < pg.height) {
+            while (options.posX < pg.width) {
+                //console.log("Adding " + this.name + " tile at " + "(" + options.posX + "," + options.posY + ")");
+                this.draw(pg, options);
+                options.posX += options.width;
             }
-        ]
+            options.posX = 0;
+            options.posY += options.height;
+        }
     }
 }
 
+
+class Config {
+    constructor() {
+        const self = this;
+        self.canvas = {
+            height: 800,
+            width: 800
+        };
+
+        self.bowl = {
+            width: self.canvas.width * 0.375,
+            height: self.canvas.height * 0.375
+        }
+
+        self.palette = {
+            gradientStart: new GeneratedValue(getRandomColor),
+            gradientStop: new GeneratedValue(getRandomColor)
+        };
+
+        self.wallpaper = new WallpaperConfig(self);
+        self.possibleSubjects = [];
+    }
+}
 
 class WallpaperConfig {
     constructor(config) {
@@ -114,27 +180,62 @@ class WallpaperConfig {
     }
 }
 
+class Subject {
+    constructor(height, width, layers) {
+        this.height = height;
+        this.width = width;
+        this.layers = layers;
+        this.pg = createGraphics(width, height);
+    }
 
-class Config {
+    drawLayers() {
+        let layers = this.layers;
+        Object.keys(this.layers).forEach(function(k) {
+            layers[k].draw();
+        })
+    }
+
+    cleanup() {
+        let layers = this.layers;
+        Object.keys(this.layers).forEach(function(k) {
+            layers[k].pg.remove();
+        })
+    }
+}
+
+class BowlSubject extends Subject {
     constructor() {
-        const self = this;
-        self.canvas = {
-            height: 800,
-            width: 800
-        };
+        let size = 300;
+        super(size, size, {
+                shape: new Layer('Bowl[shape]', size, size, function(pg) {
+                    pg.noStroke();
+                    pg.fill(getRandomPaletteColor(getRandomColor()));
+                    pg.arc(pg.width / 2, 0, pg.width, pg.height, 0, 180, OPEN);
+                },),
+                pattern: new Layer('Bowl[pattern]', size, size, function(pg) {
+                    let patternName = 'polkaDot';
+                    let pattern = patterns[patternName];
+                    pg.background(getRandomPaletteColor(getRandomColor()));
+                    pattern.tile(pg);
+                }),
+                outline: new Layer('Bowl[outline]', size, size, function(pg) {
+                    pg.push();
+                    pg.noFill();
+                    pg.strokeWeight(1.5);
+                    pg.arc(pg.width / 2, 0, pg.width, pg.height, 0, 180, OPEN);
+                    pg.line(0, 1, pg.width, 1);
+                    pg.pop();
+                })
+            }
+        );
+    }
 
-        self.bowl = {
-            width: self.canvas.width * 0.375,
-            height: self.canvas.height * 0.375
-        }
-
-        self.palette = {
-            gradientStart: new GeneratedValue(getRandomColor),
-            gradientStop: new GeneratedValue(getRandomColor)
-        };
-
-        self.wallpaper = new WallpaperConfig(self);
-        self.possibleSubjects = [];
+    getGraphics() {
+        this.drawLayers();
+        let maskedImage = this.layers.pattern.getMaskedImage(this.layers.shape.pg);
+        this.pg.image(maskedImage, 0, 0);
+        this.pg.image(this.layers.outline.pg, 0, 0);
+        return this.pg;
     }
 }
 
@@ -155,6 +256,44 @@ function setup() {
     noLoop();
 }
 
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+class PolkaDotPattern extends Pattern {
+    constructor() {
+        let size = getRandomArbitrary(20, 50);
+        super('polkaDot',
+            new OptionsTemplate({
+                defaultShade: () => getRandomPaletteColor(getRandomColor()),
+                height: () => size,
+                width: () => size,
+                posX: () => 0,
+                posY: () => 0
+            })
+        );
+    }
+
+    draw(pg, options) {
+        if (options === null) {
+            options = this.optionsTemplate.getOptions();
+        }
+
+        let dotColor = getRandomPaletteColor(options.defaultShade);
+        dotColor.setAlpha(185);
+        pg.push();
+        pg.fill(dotColor);
+        pg.noStroke();
+        pg.ellipse(options.posX, options.posY, options.width, options.height);
+        pg.pop();
+    }
+}
+
+let patterns = {
+    polkaDot: new PolkaDotPattern()
+}
+
+
 function getRandomColor() {
     return color(
         random(0, 255),
@@ -163,8 +302,8 @@ function getRandomColor() {
     )
 }
 
-function maybe(fn) {
-    if (random(0, 1) > 0.5) {
+function maybe(fn, probability = 0.5) {
+    if (random(0, 1) >= probability) {
         fn();
     }
 }
@@ -183,57 +322,20 @@ function tileShape(shape) {
     }
 }
 
-function generateBowlPattern() {
-    let p = createGraphics(config.bowl.width, config.bowl.height);
-    p.background(getRandomPaletteColor(getRandomColor()));
-    let wt = random(10, 55);
-    let numDots = random(5, 25);
-    p.push();
-    p.stroke(getRandomPaletteColor(getRandomColor()));
-    p.strokeWeight(wt)
-    p.line(0, 0, config.bowl.width, 0);
-    p.pop();
-    p.push();
-    p.strokeWeight(1);
-    p.stroke('#000000')
-    p.line(1, wt/2, config.bowl.width - 1, wt/2);
-    p.pop();
-    let pc = new PatternConfig();
-    pc.patterns[0].generator(p, -p.width, wt * 2);
-    return p;
-}
-
 function paintBowl() {
-    let bowlStartX = random(width * .25, width * .55);
-    let bowlStartY = 500;
-    let bowlHeight = 300;
-    let bowlWidth = 300;
-    let bowlShadowWidth = bowlWidth * 1.1;
-    let bowlShadowHeight = bowlHeight / 5;
-    let pattern = generateBowlPattern();
-    let bowlPattern = createGraphics(bowlWidth, bowlHeight);
-    let bowlOutline = createGraphics(bowlWidth, bowlHeight);
+    let bowl = new BowlSubject();
+    let bowlShadowWidth = bowl.width * 1.1;
+    let bowlShadowHeight = bowl.height / 5;
     let bowlShadow = createGraphics(bowlShadowWidth, bowlShadowHeight);
     let shadowColor = color('#333')
-    let pImg;
+
     shadowColor.setAlpha(90);
     bowlShadow.noStroke();
     bowlShadow.fill(shadowColor)
     bowlShadow.ellipse(bowlShadowWidth /2, bowlShadowHeight / 2, bowlShadowWidth, bowlShadowHeight);
-    bowlPattern.arc(bowlWidth / 2, 0, bowlWidth, bowlHeight, 0, 180, OPEN);
-    bowlOutline.noFill();
-    bowlOutline.strokeWeight(1.5)
-    bowlOutline.arc(bowlWidth / 2, 0, bowlWidth, bowlHeight, 0, 180, OPEN);
-    bowlOutline.line(0, 0, bowlWidth, 0);
-    ( pImg = pattern.get() ).mask( bowlPattern.get() );
-    image(bowlShadow, bowlStartX /2, bowlStartY + (bowlHeight/3));
-    image(pImg, bowlStartX, bowlStartY);
-    image(bowlOutline, bowlStartX, bowlStartY);
-    push();
-    pop();
-    bowlPattern.remove();
-    bowlOutline.remove();
-    pattern.remove();
+    let pg = bowl.getGraphics()
+    image(bowlShadow, 125,600);
+    image(pg, 250, 500);
     bowlShadow.remove();
 }
 
@@ -257,12 +359,6 @@ function paintWallpaper() {
 }
 
 function postDraw() {
-    const loadingIndicator = document.getElementById('loading');
-    const readyIndicator = document.getElementById('ready');
-    loadingIndicator.classList.toggle('visible', false);
-    loadingIndicator.classList.toggle("invisible", true);
-    readyIndicator.classList.toggle('invisible', false);
-    readyIndicator.classList.toggle("visible", true);
     let backgroundColor = config.palette.gradientStart.getValue();
     backgroundColor.setAlpha(50);
     document.getElementById('body').style.backgroundColor = backgroundColor.toString();
@@ -272,18 +368,8 @@ function postDraw() {
     document.getElementById('loading').classList.toggle('d-none');
 }
 
-function preDraw() {
-    const loadingIndicator = document.getElementById('loading');
-    const readyIndicator = document.getElementById('ready');
-    loadingIndicator.classList.toggle('invisible', false);
-    loadingIndicator.classList.toggle('visible', true);
-    readyIndicator.classList.toggle('visible', false);
-    readyIndicator.classList.toggle('invisible', true);
-}
-
 function draw() {
     if (!generating) {
-        preDraw();
         console.log("Generating image using configuration: ")
         console.log(config);
         generating = true;
@@ -293,7 +379,6 @@ function draw() {
         paintBowl(backgroundColor);
     }
     generating = false;
-    console.log("Generation complete.");
     postDraw();
 }
 
